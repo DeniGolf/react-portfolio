9add8ea1e805b2ff207bf8bf3ce3b6cd6b1766b2
Denis <denis.klemeshov@gmail.com>
2021-03-03 22:00:18 +0200

    project uploaded

---
+++ src/components/FaqSection.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+//Styled
+import styled from "styled-components";
+//Styles
+import { About } from "../styles";
+import Toggle from "./Toggle";
+//Animation
+import { AnimateSharedLayout } from "framer-motion";
+import { scrollReveal } from "../animation";
+//Scroll Animation
+import { useScroll } from "./useScroll";
+
+const FaqSection = () => {
+  const [element, controls] = useScroll();
+  return (
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae,
+              iusto?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae,
+              iusto?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Diferrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae,
+              iusto?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae,
+              iusto?
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+    </Faq>
+  );
+};
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
+
+export default FaqSection;
---
+++ src/components/useScroll.js
@@ -0,0 +1,15 @@
+//Intersection Observer
+import { useInView } from "react-intersection-observer";
+//Animation
+import { useAnimation } from "framer-motion";
+
+export const useScroll = () => {
+  const controls = useAnimation();
+  const [element, view] = useInView({ threshold: 0.3 });
+  if (view) {
+    controls.start("show");
+  } else {
+    controls.start("hidden");
+  }
+  return [element, controls];
+};
